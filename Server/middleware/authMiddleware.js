722d4f19a99047f27304b36e12275afd3448d8bf
Prathamesh Doni <creatives.doni@gmail.com>
2023-09-17 14:17:12 +0530

    last changes

--- Server/middleware/authMiddleware.js
+++ Server/middleware/authMiddleware.js
@@ -13,7 +13,7 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       //decodes token id
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.jwt_secret);
 
       req.user = await User.findById(decoded.id).select("-password");
 
