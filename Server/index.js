3973948b9d714c735a4323607f1125dbb0f87dab
Prathamesh Doni <91219171+hacknovas@users.noreply.github.com>
2023-09-21 20:57:01 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -32,7 +32,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://frontend:3000/",
+    origin: "http://frontend:31000/",
   },
 });
 
